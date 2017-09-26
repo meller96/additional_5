5fab33aaf396a6f9aeaefd5194dcc2b500f560b3
meller96 <meller70@gmail.com>
2017-09-26 18:34:52 +0300

    update brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports =  function check(str, bracketsConfig) {
+    if (str.length == 0)
+        return true;
+    for (var i = 0; i < bracketsConfig.length; i++) {
+        var firstIndex = str.indexOf(bracketsConfig[i][0]);
+        var secondIndex = str.indexOf(bracketsConfig[i][1], firstIndex + 1);
+        if (firstIndex == -1 && secondIndex == -1)
+            continue;
+        if (firstIndex == -1 || secondIndex == -1)
+            return false;
+        while (secondIndex != -1) {
+            if (check(str.substr(firstIndex + 1, secondIndex-firstIndex - 1), bracketsConfig) && check(str.substr(0, firstIndex) + str.substr(secondIndex+1), bracketsConfig))
+                break;
+            secondIndex = str.indexOf(bracketsConfig[i][1], secondIndex + 1);
+        }
+        if (secondIndex == -1)
+            return false;
+    }
+    return true;       
 }
+ 
+
